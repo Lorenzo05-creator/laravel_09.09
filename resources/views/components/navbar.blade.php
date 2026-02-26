fa6bc9cc735cc0d0c2fd3aff775b3f4e81245b6b
Lorenzo05-creator <lory4jz@gmail.com>
2026-02-26 11:44:50 +0100

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,9 @@
 
+
 <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
 <div class="container">
 <a class="navbar-brand" href="{{ route('home') }}">My Blog</a>
 <a class="btn btn-outline-light" href="{{ route('posts.index') }}">Post</a>
+<a class="btn btn-success ms-2" href="{{ route('posts.create') }}">Crea Post</a>
 </div>
 </nav>
